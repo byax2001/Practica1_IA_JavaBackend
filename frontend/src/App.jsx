49e4dd0aaf61a1cccac2692373ef7e0472238a58
MoonGSK <3043441920114@ingenieria.usac.edu.gt>
2024-02-17 15:25:58 -0600

    Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Route,BrowserRouter,Routes,HashRouter} from 'react-router-dom';
+import Homepage from './pages/Homepage';
 import './App.css'
 
 export default  function App(){
 
   return(
-    <>
-      <h1>HOLA QUE TAL</h1>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path='/' exact element={<Homepage/>}/>
+      </Routes>
+    </HashRouter>
   )
 }
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+
+export default function Homepage(){
+    return(
+        <Grid
+            container
+            sx={{ display: "flex", fontFamily: "Arial" }}
+        >
+            <h1>hola</h1>
+
+        </Grid>
+    )
+
+}
