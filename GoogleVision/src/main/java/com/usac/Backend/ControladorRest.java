31ff14dd25c44a18811b4ed160b491865db09715
byax <3043441920114@ingenieria.usac.edu.gt>
2024-02-22 23:18:49 -0600

    prasf

---
+++ GoogleVision/src/main/java/com/usac/Backend/ControladorRest.java
@@ -0,0 +1,102 @@
+package com.usac.Backend;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+
+
+
+@RestController
+@Slf4j
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class ControladorRest {
+    @GetMapping("/")
+    public String Comienzo() {
+        log.info("Peticion GET - Inicio");
+        log.debug("Información Extra: ");
+        return "Hola Mundo";
+    }
+    @GetMapping("/1")
+    public String Comienzo1() {
+        log.info("Peticion GET - Inicio");
+        log.debug("Información Extra: ");
+        return "Hola Mundo";
+    }
+
+    @PostMapping("/analisisImage")
+    @CrossOrigin(origins = "http://localhost:4000")
+    public List<String> analizarImagen(MultipartFile imageFile) throws IOException {
+        List<String> results = new ArrayList<>();
+        if (imageFile == null) {
+            results.add("Se envió un archivo nulo");
+            return results;
+        }
+
+        byte[] imageB = imageFile.getBytes();
+        String base64Image = Base64.getEncoder().encodeToString(imageB);
+        String keyV = "AIzaSyAz3G7fw2ZYe9rTBtPusiEZshRrP4P02JU"; // Agrega tu clave API aquí
+        String url = "https://vision.googleapis.com/v1/images:annotate?key=" + keyV;
+
+        /*
+        * LABEL_DETECTION (detección de etiquetas) es una función de la API de Google Vision que identifica objetos, lugares, actividades, productos y otros elementos significativos en una imagen. Básicamente, proporciona una lista de etiquetas que describen el contenido de la imagen.
+            Por ejemplo, si tienes una imagen de una playa, el LABEL_DETECTION podría devolver etiquetas como "playa", "mar", "arena", "cielo", "personas", etc.
+        * TEXT_DETECTION: Para detectar texto en la imagen.
+        * FACE_DETECTION: Para detectar rostros en la imagen.
+        * LANDMARK_DETECTION: Para detectar puntos de referencia en la imagen.
+        * LOGO_DETECTION: Para detectar logotipos en la imagen.
+        * DOCUMENT_TEXT_DETECTION: Para detectar texto en documentos dentro de la imagen.
+        * IMAGE_PROPERTIES: Para obtener propiedades de la imagen, como colores dominantes.
+        * SAFE_SEARCH_DETECTION: Para detectar contenido inapropiado en la imagen.
+        */
+        String jsonBody = "{"
+                + "\"requests\":["
+                + "{"
+                + "\"image\":{"
+                + "\"content\":\"" + base64Image + "\""
+                + "},"
+                + "\"features\":["
+                + "{"
+                + "\"type\":\"FACE_DETECTION\""
+                + "},"
+                + "{"
+                + "\"type\":\"SAFE_SEARCH_DETECTION\""
+                + "}"
+                + "]"
+                + "}"
+                + "]" + "}";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(jsonBody, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, requestEntity, String.class);
+
+        HttpStatus statusCode = (HttpStatus) responseEntity.getStatusCode();
+        if (statusCode == HttpStatus.OK) {
+            String responseBody = responseEntity.getBody();
+            System.out.println("Respuesta del servidor: " + responseBody);
+            results.add(responseBody);
+        } else {
+            System.err.println("La solicitud no se pudo completar. Código de estado: " + statusCode);
+        }
+        return results;
+    }
+}
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,23 +1,63 @@
 import { Grid, Typography,TextareaAutosize, FormControl } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 import { useState,useEffect,useContext, useRef} from "react";
 import CustomTextField from "../components/CustomTextField/CustomTextField";
 import ColorButton from "../components/ColorButtom/ColorButton";
+import axios from "axios";
+
+
+const TableComponent = ({Data}) => {
+  return (
+    <TableContainer sx={{marginTop:5}} component={Paper}>
+      <Table sx={{}}>
+        <TableHead>
+          <TableRow sx={{backgroundColor:"#434343"}}>
+            <TableCell sx={{color:"white"}}>Category</TableCell>
+            <TableCell sx={{color:"white"}}>Value</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {Object.keys(Data).map((category, index) => (
+            <TableRow key={index}>
+              <TableCell>{category}</TableCell>
+              <TableCell>{Data[category]}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+
+};
 
 export default function Homepage() {
-const [infoImage, setInfoImage] = useState('');
-const [image, setImage] = useState('');
+  const Data = {
+    adult: "VERY_UNLIKELY",
+    medical: "VERY_UNLIKELY",
+    racy: "UNLIKELY",
+    spoof: "UNLIKELY",
+    violence: "VERY_UNLIKELY"
+  };
+const [infoImage, setInfoImage] = useState({
+  adult: "",
+  medical: "",
+  racy: "",
+  spoof: "",
+  violence: ""
+});
+const [nCaras, setNCaras] = useState("0")
+const [image, setImage] = useState('https://science.nasa.gov/wp-content/uploads/2023/06/spiral-galaxy-jpg.webp');
 const fileInputRef = useRef(null); // Crear una referencia al input de tipo file
 // con el useRef se puede jalar el valor de un input creado en otra clase y colocarlo adentro de otra variable desada
 
 const handleCargarClick = () => {
     const file = fileInputRef.current.files[0]; // Obtener el archivo seleccionado
-
     if (file) {
       // Leer el contenido del archivo seleccionado
       const reader = new FileReader();
       reader.onload = (event) => {
         setImage(event.target.result)
-        console.log('Contenido del archivo:', event.target.result); //formato 64bit
+        //console.log('Contenido del archivo:', event.target.result); //formato 64bit
       };
       reader.readAsDataURL(file);
     } else {
@@ -28,7 +68,33 @@ const handleCargarClick = () => {
   const handleProcesarClick = () => {
     const file = fileInputRef.current.files[0]; // Obtener el archivo seleccionado
     if (file) {
-      console.log('Nombre del archivo:', file.name);
+      const formData = new FormData();
+      formData.append('imageFile', file);
+
+      axios.post('http://localhost:4000/analisisImage', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+        .then(response => {
+          //console.log('Respuesta del servidor:', response.data);
+          let jsonClean = response.data[0].replace(/\n/g, "") //se eliminan saltos de linea
+          jsonClean = jsonClean.replace(/\\\"/g,"\"") // se eliminan \" por "
+          let trueJson = JSON.parse(jsonClean) 
+          let datos = trueJson["responses"][0]
+          if(datos.hasOwnProperty('faceAnnotations')){
+            setNCaras(datos["faceAnnotations"].length)
+          }else{
+            setNCaras("No se detectaron Rostros")
+          }
+          setInfoImage(datos["safeSearchAnnotation"])
+          //console.log(jsonClean)
+          //setInfoImage(trueJson)
+          console.log(datos)
+        })
+        .catch(error => {
+          console.error('Error al enviar el archivo:', error);
+        });
     } else {
       console.log('No se ha seleccionado ningún archivo.');
     }
@@ -79,14 +145,24 @@ const handleCargarClick = () => {
             donde handleInputChange seria una funcion cualquiera que cambia los valores de la variable a través
             del uso de setVariable declarado en el useEffect, el handleInputChange deberia de ser handleInputChange(event){event.target.value}
           */}
-          <TextareaAutosize
-          id="textarea"
-          aria-label="Descripción"
-          minRows={3}
-          placeholder="Escribe aquí..."
-          style={{ width: '100%' }}
-          value={infoImage}
-        />
+          <Grid container>
+            <CustomTextField
+              sx={{ width: "80%", marginTop: 5, marginLeft: 5 }}
+              type="text"
+              inputProps={{
+                // accept: "image/*", //se aceptaran todos los archivos que sean de tipo imagen
+                accept:".jpg, .png"
+              }}
+              helperText="Numero de Rostros"
+              FormHelperTextProps={{
+                style: { color: "#B2BAC2" }, // Cambia el color del texto de ayuda a blanco
+              }}
+              name="numberFaces"
+              value={String(nCaras)}
+            />
+          </Grid>
+          
+        <TableComponent Data={infoImage}/>
         </Grid>
         <Grid item xs={1} />
       </Grid>
